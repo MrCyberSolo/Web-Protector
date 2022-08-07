b5ea88dfb9b53b328a30090fa782b587df025fb3
Naem Azam <78720392+naemazam@users.noreply.github.com>
2022-08-07 20:24:52 +0800

    Add files via upload

---
+++ pages/footer.php
@@ -0,0 +1,12 @@
+    <br /></div>
+      <footer>
+        <div class="mt-auto d-flex justify-content-center">
+          <div class="col-lg-8">
+            <br />
+            <p>Protected by <strong>Web Protector</strong></p>
+          </div>
+        </div>
+      </footer>
+
+  </body>
+</html>
